e156239955b8658594d37c7db830c4dc48963c2f
LinkDevArch <jstivenbedoya@estudiante.uniajc.edu.co>
2025-02-04 18:06:21 -0500

    Standard class

--- src/main/java/net/johan/firstmod/FirstMod.java
+++ src/main/java/net/johan/firstmod/FirstMod.java
@@ -1,6 +1,11 @@
 package net.johan.firstmod;
 
 import com.mojang.logging.LogUtils;
+import net.johan.firstmod.block.ModBlocks;
+import net.johan.firstmod.items.ModItems;
+import net.johan.firstmod.utils.CustomItemProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -15,22 +20,23 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(FirsteMod.MOD_ID)
-public class FirsteMod
+@Mod(FirstMod.MOD_ID)
+public class FirstMod
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "firstmod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public FirsteMod(FMLJavaModLoadingContext context)
+    public FirstMod(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -50,7 +56,19 @@ public class FirsteMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.COMBAT)
+        {
+            event.accept(ModItems.GODWEAPON);
+            event.accept(ModItems.GODBOW);
+        }
+        if (event.getTabKey()==CreativeModeTabs.NATURAL_BLOCKS) {
+            event.accept((ModBlocks.DIAMANTIUS_ORE));
+            event.accept((ModBlocks.DEEPSLATE_DIAMANTIUS_ORE));
+        }
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.DIAMANTIUS);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -67,7 +85,7 @@ public class FirsteMod
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event)
         {
-
+            CustomItemProperties.addCustomItemProperties();
         }
     }
 }
