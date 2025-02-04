24bf3abc9039cb159e1448eed7e5b7992b4bfbd3
LinkDevArch <jstivenbedoya@estudiante.uniajc.edu.co>
2025-02-04 18:03:19 -0500

    Custom Properties Class

--- src/main/java/net/johan/firstmod/utils/CustomItemProperties.java
+++ src/main/java/net/johan/firstmod/utils/CustomItemProperties.java
@@ -1,15 +1,56 @@
 package net.johan.firstmod.utils;
 
+import net.johan.firstmod.items.ModItems;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 
-public class GodBowItemProperties  {
+public class CustomItemProperties extends BowItem {
+
+    public CustomItemProperties(Properties properties) {
+        super(properties);
+    }
 
     public static void addCustomItemProperties() {
-        ItemProperties prop = new ItemProperties("godbow");
+        customBow(ModItems.GODBOW.get());
+    }
+
+    public static void customBow(Item item){
+        ItemProperties.register(item, new ResourceLocation("pull"), (p_174635_, p_174636_, p_174637_, p_174638_) -> {
+            if (p_174637_ == null) {
+                return 0.0F;
+            } else {
+                return p_174637_.getUseItem() != p_174635_ ? 0.0F : (float)(p_174635_.getUseDuration() - p_174637_.getUseItemRemainingTicks()) / 20.0F;
+            }
+        });
+        ItemProperties.register(item, new ResourceLocation("pulling"), (p_174630_, p_174631_, p_174632_, p_174633_) -> {
+            return p_174632_ != null && p_174632_.isUsingItem() && p_174632_.getUseItem() == p_174630_ ? 1.0F : 0.0F;
+        });
+    }
+
+    @Override
+    public void releaseUsing(ItemStack stack, Level level, LivingEntity entity, int timeLeft) {
+        if (entity instanceof Player player) {
+            int chargeTime = this.getUseDuration(stack) - timeLeft; // Tiempo que se cargó el arco
+            float power = getPowerForTime(chargeTime);
+
+            if (!level.isClientSide && power > 0.1F) {
+                AbstractArrow arrow = new Arrow(level, player);
+                arrow.setBaseDamage(25.0F); // Daño doble
+                arrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, power * 3.0F, 1.0F);
+
+                level.addFreshEntity(arrow);
+                System.out.println(arrow.getBaseDamage());
+            }
+        }
     }
 
 }
